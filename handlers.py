b71fd050e8e161cf362973d16b70abd3d3e52225
73nyaya <nicolasyayat@gmail.com>
2024-02-16 15:34:38 -0500

    adding printing errors to folder creation

--- handlers.py
+++ handlers.py
@@ -294,7 +294,11 @@ def handle_project_status_changed(data: dict) -> None:
     project_status = data.get('customStatusId')
     project_id = data.get('taskId')
     if project_status == 'IEAEINT7JMCLXBAC':
-        create_project_folder(project_id=project_id)
+        print('Creating project folder')
+        try:
+            create_project_folder(project_id=project_id)
+        except Exception as e:
+            print('Failed to create project, unexpected error: ', e)
     deal_id = get_key_from_value(get_objects_translator(), project_id)
     if deal_id:
         deal = Deal(deal_id=deal_id)
